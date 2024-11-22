c8712f0ed40c5f5fa7823f4b42b5da06b3048a1f
Alan Torres De Sá <115950460+desaats@users.noreply.github.com>
2024-11-22 19:24:59 -0300

    Models

---
+++ src/backend/Models/TB_VENDAS_PRODUTOS.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace sonhopadaria.Models
+{
+    public class TB_VENDAS_PRODUTOS
+    {
+        [Key]
+        public int venda_prod_id { get; set; }
+
+        [Required]
+        public int produto_id { get; set; }
+
+        [Required]
+        public int venda_id { get; set; }
+
+        [Required]
+        public double preco { get; set; }
+
+        [Required]
+        public int quantidade { get; set; }
+
+        [Required]
+        public int etiqueta_id { get; set; }
+    }
+}
